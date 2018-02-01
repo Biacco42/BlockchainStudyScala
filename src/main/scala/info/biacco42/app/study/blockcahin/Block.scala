6191fd3c3ad65771e536b86bda273230c2507957
Biacco42 <biacco42@gmail.com>
2018-02-02 03:21:30 +0900

    Add case classes to represent Block

--- src/main/scala/info/biacco42/app/study/blockcahin/Block.scala
+++ src/main/scala/info/biacco42/app/study/blockcahin/Block.scala
@@ -1,7 +1,15 @@
 package info.biacco42.app.study.blockcahin
 
-case class Block()
+case class Block(header: BlockHeader, transactions: List[Transaction])
 
-case class BlockHeader()
+case class BlockHeader(previousHeaderHash: String, merkleRoot: String, difficulty: Int, nonce: Int)
 
-case class Transaction()
+case class Transaction(id: String, inputs: List[TransactionInput], outputs: List[TransactionOutput])
+
+case class TransactionInput(transactionID: String, outputIndex: Int, signatureScript: SignatureScript)
+
+case class SignatureScript(publicKey: String, signature: String)
+
+case class TransactionOutput(amount: Int, publicKeyScript: PublicKeyScript)
+
+case class PublicKeyScript(publicKeyHash: String)
